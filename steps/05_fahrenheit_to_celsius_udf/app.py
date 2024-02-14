75e2de4196e6752db687f021d183c470853e7e90
Kiran Rimmalapudi <505058232@penske.com>
2024-02-13 16:14:01 -0800

    update

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         print(main(*sys.argv[1:]))  # type: ignore
     else:
-        print(main())  # type: ignore
+        print(main())  # type: ignore 
